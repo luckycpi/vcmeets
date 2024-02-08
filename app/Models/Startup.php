57765d4017aea58be5535bd198f37012dd4bf6a8
Lucky <lucky@cpibusiness.net>
2024-02-08 11:27:49 +0400

    Adding soft deletes

--- app/Models/Startup.php
+++ app/Models/Startup.php
@@ -4,10 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Startup extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
 
     public function votes() {
         return $this->hasMany(StartupVotes::class)->where('user_id', auth()->id());
